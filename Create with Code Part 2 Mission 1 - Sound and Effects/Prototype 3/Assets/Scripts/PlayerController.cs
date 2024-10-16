a7308abdcf9e0dbc07897f9d8be65909f260d88f
nbeattz <nbeattie.film@gmail.com>
2024-10-16 17:10:07 +0100

    Completed 3.2 - Prototype 3

--- Create with Code Part 2 Mission 1 - Sound and Effects/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 2 Mission 1 - Sound and Effects/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +25,12 @@ public class PlayerController : MonoBehaviour
         } }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over!");
+        }
     }
 }
